b98a9cb419cf42ae3a844d354b82375cc8e7c4b8
chessyBoom <kwrrwytinii24@gmail.com>
2023-02-13 21:46:44 +0800

    增加分类信息

--- src/api/article-info/controllers/article-info.js
+++ src/api/article-info/controllers/article-info.js
@@ -11,10 +11,18 @@ module.exports = createCoreController(
   "api::article-info.article-info",
   ({ strapi }) => ({
     async find(ctx) {
+      ctx.query = {
+        ...ctx.query,
+        populate: "deep",
+      };
       const { data, meta } = await super.find(ctx);
       return { data: removeAttrsAndId(removeTime(data)), meta };
     },
     async findOne(ctx) {
+      ctx.query = {
+        ...ctx.query,
+        populate: "deep",
+      };
       const { data, meta } = await super.findOne(ctx);
       return removeAttrsAndId(removeTime(data));
     },
