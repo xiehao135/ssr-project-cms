d6d06e7b4922b007e916067efd28d9e123357d23
unknown <wlijun0808@163.com>
2023-02-20 23:35:35 +0800

    update

---
+++ src/api/author-info/services/author-info.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * author-info service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::author-info.author-info');
